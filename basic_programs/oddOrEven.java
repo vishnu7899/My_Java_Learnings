81873976562fff94095a2625c8088a3e80d21bdc
Vishnu <polepallivishnu78@gmail.com>
2025-02-09 21:47:42 +0530

    Even or odd program added on 09/Feb/2025

---
+++ basic_programs/oddOrEven.java
@@ -0,0 +1,13 @@
+public class oddOrEven {
+
+    public static void main(String[] args) {
+        int num =5;
+        
+        if(num%2==0){
+            System.out.println(num+ " is Even ");
+        }
+        else{
+            System.out.println(num+ " is Odd");
+        }
+    }
+}
