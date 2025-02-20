bbf4b124fd3cd8bff0ba874c2df4974a949a4804
Vishnu <polepallivishnu78@gmail.com>
2025-02-20 21:22:20 +0530

    Basic program of array added

---
+++ Topicwise_concepts/Arrays/ArrayConcept.java
@@ -0,0 +1,26 @@
+package Topicwise_concepts.Arrays;
+
+import java.util.Scanner;
+
+public class ArrayConcept {
+    public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the size of the Array:");
+       int size = sc.nextInt();
+       int numbers[] = new int[size];
+
+
+       for(int i=0; i<size; i++) {
+           numbers[i] = sc.nextInt();
+       }
+
+
+       //print the numbers in array
+       System.out.print("Array elements are: ");
+       for(int i=0; i<numbers.length; i++) {
+           System.out.print(numbers[i]+" ");
+       }
+       sc.close();
+   }
+
+}
