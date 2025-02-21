5d47b3c562230d934e9b74176ac1d1058e49dd2f
Vishnu <polepallivishnu78@gmail.com>
2025-02-21 06:40:27 +0530

    Factorial program using recursion added

---
+++ TopicWiseConcepts/Recursion/fact.java
@@ -0,0 +1,16 @@
+package TopicWiseConcepts.Recursion;
+
+public class fact {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of: " +num+ " is: "+factorial(num));
+    }
+    static int factorial(int n){
+        if(n==0){
+            return 1;
+        }
+        else{
+            return (n*factorial(n-1));
+        }
+    }
+}
