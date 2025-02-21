d510f6f801803848283d1e4e46f8195642687778
Vishnu <polepallivishnu78@gmail.com>
2025-02-21 06:29:28 +0530

    Fibbonaci program added

---
+++ TopicWiseConcepts/Recursion/fibs.java
@@ -0,0 +1,20 @@
+package TopicWiseConcepts.Recursion;
+
+public class fibs {
+    public static void main(String[] args) {
+       int Recursion =  fib(5);
+
+        System.out.println(Recursion);
+    }
+
+    static int fib(int n){
+        if(n<=1){
+            return n;
+        }
+
+        return (fib(n-1) + fib(n-2));
+        
+    }
+}
+   
+
